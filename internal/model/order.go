c4d461c83e0bc44e01627aabc4c15ff6c635369a
aakritigkmit <aakriti@gkmit.co>
2025-04-09 12:29:59 +0530

    feat:add order model

--- internal/model/order.go
+++ internal/model/order.go
@@ -8,11 +8,12 @@ import (
 
 type Order struct {
 	ID                     primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	UserID                 string             `bson:"user_id"`
-	MerchantOrderReference int                `bson:"merchant_order_reference"`
+	UserID                 string             `bson:"userId"`
+	MerchantOrderReference int                `bson:"merchantOrderReference"`
+	PineToken              string             `bson:"pineToken"`
 	Amount                 float32            `bson:"amount"`
 	Currency               float32            `bson:"currency"`
 	Status                 string             `bson:"status"` // pending, success, failure
-	CreatedAt              time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	CreatedAt              time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
 	UpdatedAt              time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
 }
